706bd352e892b9d03f5287846c083100fecff0ef
earthmessenger <me@earthmessenger.xyz>
2023-10-01 12:53:04 +0800

    添加动画

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,17 @@ const canvasContainer = document.querySelector<HTMLDivElement>("#canvas-containe
 const canvas = document.querySelector<HTMLCanvasElement>("#canvas")!;
 
 interface GameConfig {
-    column: number,
-    keys: Array<string>,
-    minHWRatio: number,
+	column: number,
+	keys: Array<string>,
+	minHWRatio: number,
+	accelY: number,
 }
 
 const config: GameConfig = {
 	column: 4,
 	keys: ["KeyD", "KeyF", "KeyJ", "KeyK"],
 	minHWRatio: 2.0,
+	accelY: 0.05,
 };
 
 const beatmap: Array<Array<boolean>> = [];
@@ -32,8 +34,6 @@ const updateSize = () => {
 updateSize();
 window.onresize = updateSize;
 
-let currentRow = 0;
-
 const generateBeatmap = () => {
 	const res = new Array<boolean>(config.column);
 	res[Math.floor(Math.random() * config.column)] = true;
@@ -47,20 +47,33 @@ const getRow = (row: number) => {
 	return beatmap[row];
 };
 
+let currentRow = 0;
+let floatY = 0.0;
+let velY = 0.0;
+
 const redrawCanvas = () => {
 	const { height, width } = canvas;
-	const { column } = config;
+	const { column, accelY } = config;
 	const ctx = canvas.getContext("2d")!;
 	const blockSize = Math.floor(width / column);
 	const row = Math.ceil(height / blockSize);
 
 	ctx.clearRect(0, 0, width, height);
 
+
+	if (floatY <= 0.0) {
+		floatY = 0.0;
+		velY = 0.0;
+	} else {
+		floatY -= velY;
+		velY += accelY;
+	}
+
 	for (let i = 0; i < row; i++) {
 		const r = getRow(i + currentRow);
 		for (let j = 0; j < column; j++) {
 			if (r[j]) {
-				ctx.fillRect(j * blockSize, height - (i + 1) * blockSize, blockSize, blockSize);
+				ctx.fillRect(j * blockSize, height - (i + 1) * blockSize - floatY * blockSize, blockSize, blockSize);
 			}
 		}
 	}
@@ -77,7 +90,10 @@ const handleKeys = (e: KeyboardEvent) => {
 	const r = getRow(currentRow);
 	if (r[pressedColumn]) {
 		r[pressedColumn] = false;
-		if (r.every((e) => !e)) currentRow++;
+		if (r.every((e) => !e)) {
+			currentRow++;
+			floatY = 1.0;
+		}
 	}
 };
 
